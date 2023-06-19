6baf289d195c914e8a5471c243043a2039bf41ab
Serhey325 <sabserj2017@gmail.com>
2023-06-19 17:09:27 +0200

    optimization FifthSectionItem

--- app/components/homePage/FifthSectionItem.tsx
+++ app/components/homePage/FifthSectionItem.tsx
@@ -1,38 +1,74 @@
 "use client"
 
-import React, {useState} from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 type Props = {
-    src_gray: string
-    src_purple: string
-    lastOne?: boolean
-}
+    src_gray: string;
+    src_purple: string;
+    lastOne?: boolean;
+};
 
 interface Position {
     x: number;
     y: number;
 }
-const FifthSectionItem = ({src_gray, src_purple, lastOne}: Props) => {
-    const [position, setPosition] = useState<Position>({x: 0, y: 0});
+
+const FifthSectionItem = ({ src_gray, src_purple, lastOne }: Props) => {
+    const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+    const divRef = useRef<HTMLDivElement>(null);
 
     const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-        const parentRect = e.currentTarget.getBoundingClientRect();
-        const offsetX = e.clientX - parentRect.left;
-        const offsetY = e.clientY - parentRect.top;
-
-        setPosition({ x: offsetX/5, y: offsetY/4 });
+        console.log("eventWorking")
+        const parentRect = divRef.current?.getBoundingClientRect();
+        if (parentRect) {
+            const offsetX = e.clientX - parentRect.left;
+            const offsetY = e.clientY - parentRect.top;
+            setPosition({ x: offsetX / 5, y: offsetY / 4 });
+        }
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 1024) {
+                // @ts-ignore
+                divRef?.current?.addEventListener("mousemove", handleMouseMove);
+            } else {
+                // @ts-ignore
+                divRef?.current?.removeEventListener("mousemove", handleMouseMove);
+            }
+        };
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            // @ts-ignore
+            divRef?.current?.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <div
-            className={`group relative flex justify-center items-center ${lastOne ? "py-6" : "py-10"} xl:py-15 2xl:py-20`}
-            onMouseMove={handleMouseMove}
+            ref={divRef}
+            className={`group relative flex justify-center items-center ${
+                lastOne ? "py-6" : "py-10"
+            } xl:py-15 2xl:py-20`}
         >
-            <img src={src_gray} alt="Section5" className={`absolute ${lastOne ? "w-[20%] md:w-[40%]" : "w-[40%]"} group-hover:opacity-30`}/>
+            <img
+                src={src_gray}
+                alt="Section5"
+                className={`absolute ${
+                    lastOne ? "w-[20%] md:w-[40%]" : "w-[40%]"
+                } group-hover:opacity-30`}
+            />
             <img
                 src={src_purple}
                 alt="Section5"
-                className={`hidden lg:block ${lastOne ? "w-[20%] md:w-[40%]" : "w-[40%]"} opacity-0 z-50 transition-transform absolute group-hover:opacity-100 translate-x-[50%] lg:translate-y-[20%] xl:translate-y-[10%] 2xl:translate-y-[30%] 3xl:translate-y-[10%]`}
+                className={`hidden lg:block ${
+                    lastOne ? "w-[20%] md:w-[40%]" : "w-[40%]"
+                } opacity-0 z-50 transition-transform absolute group-hover:opacity-100 translate-x-[50%] lg:translate-y-[20%] xl:translate-y-[10%] 2xl:translate-y-[30%] 3xl:translate-y-[10%]`}
                 style={{
                     left: `${position.x}px`,
                     top: `${position.y}px`,
