6d233dd1c7233c97bee4d2c6a37a939b917582d3
Serhey325 <sabserj2017@gmail.com>
2023-07-05 22:46:42 +0200

    refactoring ProductCard

--- app/(pages)/store/components/ProductCard.tsx
+++ app/(pages)/store/components/ProductCard.tsx
@@ -52,8 +52,24 @@ const ProductCard = ({data: product, currentUser}: Props) => {
                 {product.title}
             </div>
             <div className="flex flex-row items-center gap-1">
-                <div className="font-semibold text-2xl my-[-15px] text-gray-300">
-                    $ {product.price}
+                <div className="font-semibold text-2xl my-[-15px] text-gray-300 flex gap-2">
+                    $ {(product.sale ? product.price - product.price/100 * product.sale : product.price).toFixed(1)}
+                    {
+                        product.sale &&
+                        <span
+                            className="
+                            relative
+                            text-sm text-gray-400
+                            before:absolute before:inset-0
+                            before:-rotate-[12deg]
+                            before:translate-y-2 before:translate-x-1
+                            before:border-t-2 before:border-red-500
+                            beforer:w-full
+                        "
+                        >
+                            {product.price.toFixed(2)}
+                        </span>
+                    }
                 </div>
             </div>
             <div className="font-light text-sm text-gray-400 justify-self-start mb-[-10px] ">
